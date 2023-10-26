2231483587cabf6e9ca28df63f489823f6c60b54
pedrohrfranca <pedrof_bh@hotmail.com>
2023-10-26 16:01:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// DATA TOPO
+
+var dataHojeElement = document.getElementById("data-hoje");
+
+var dataAtual = new Date();
+
+var options = { year: 'numeric', month: 'long', day: 'numeric' };
+var dataFormatada = dataAtual.toLocaleDateString('pt-BR', options);
+
+dataHojeElement.textContent = dataFormatada;
