20eaa6e27b0d7a613d69a28206331a31f1bd985c
pedrohrfranca <pedrof_bh@hotmail.com>
2023-10-27 07:31:01 -0300

    first JS steps

--- main.js
+++ main.js
@@ -8,3 +8,38 @@ var options = { year: 'numeric', month: 'long', day: 'numeric' };
 var dataFormatada = dataAtual.toLocaleDateString('pt-BR', options);
 
 dataHojeElement.textContent = dataFormatada;
+
+// ABRIR ATIVIDADES
+
+document.addEventListener("DOMContentLoaded", function () {
+    const abrirAtividades = document.querySelectorAll("[id^='abrir-atividades']");
+    const listaAtividades = document.querySelectorAll("[class^='lista-atividades']");
+
+    abrirAtividades.forEach((botao, index) => {
+        botao.addEventListener("click", () => {
+            if (listaAtividades[index].classList.contains("hidden")) {
+                listaAtividades[index].classList.remove("hidden");
+            } else {
+                listaAtividades[index].classList.add("hidden");
+            }
+        });
+    });
+});
+
+
+// CHECKBOX
+
+document.addEventListener("DOMContentLoaded", function () {
+    const checkboxes = document.querySelectorAll(".checkbox");
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener("change", function () {
+            const label = checkbox.parentElement; // O elemento pai (li) do checkbox
+            if (checkbox.checked) {
+                label.classList.add("riscado"); // Adicionar uma classe para riscar o texto
+            } else {
+                label.classList.remove("riscado"); // Remover a classe para cancelar o risco
+            }
+        });
+    });
+});
