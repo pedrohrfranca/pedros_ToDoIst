c6c31a22f3bb2be818a4405d08c5693214fd1b65
pedrohrfranca <pedrof_bh@hotmail.com>
2023-10-30 13:40:54 -0300

    add buttom-tarefas

--- main.js
+++ main.js
@@ -34,11 +34,62 @@ document.addEventListener("DOMContentLoaded", function () {
 
     checkboxes.forEach((checkbox) => {
         checkbox.addEventListener("change", function () {
-            const label = checkbox.parentElement; // O elemento pai (li) do checkbox
+            const label = checkbox.parentElement;
             if (checkbox.checked) {
-                label.classList.add("riscado"); // Adicionar uma classe para riscar o texto
+                label.classList.add("riscado");
             } else {
-                label.classList.remove("riscado"); // Remover a classe para cancelar o risco
+                label.classList.remove("riscado");
+            }
+        });
+    });
+});
+
+
+// ADICIONAR TAREFA
+
+document.addEventListener("DOMContentLoaded", function() {
+    const addTaskButton = document.getElementById("adicionar-tarefa");
+    const lateTasksList = document.querySelector(".atividades-atrasadas .lista-atividades");
+    const todayTasksList = document.querySelector(".atividades-hoje .lista-atividades");
+    const upcomingTasksList = document.querySelector(".atividades-embreve .lista-atividades");
+
+    addTaskButton.addEventListener("click", function() {
+        Swal.fire({
+            title: 'Digite a descrição da tarefa:',
+            input: 'text',
+            showCancelButton: true,
+            confirmButtonText: 'Adicionar', // Personaliza o texto do botão
+            confirmButtonColor: '#E0662F',   // Personaliza a cor de fundo do botão        
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const taskText = result.value;
+                const task = document.createElement("li");
+                task.innerHTML = `<input class="checkbox" type="checkbox">${taskText}`;
+
+                Swal.fire({
+                    title: "Escolha a lista: 'Atrasadas', 'Hoje' ou 'Em Breve'",
+                    input: 'text',
+                    showCancelButton: true,
+                    confirmButtonText: 'Incluir',
+                    confirmButtonColor: '#E0662F'
+                }).then((listResult) => {
+                    if (listResult.isConfirmed) {
+                        const listName = listResult.value.toLowerCase();
+                        switch (listName) {
+                            case 'atrasadas':
+                                lateTasksList.appendChild(task);
+                                break;
+                            case 'hoje':
+                                todayTasksList.appendChild(task);
+                                break;
+                            case 'em breve':
+                                upcomingTasksList.appendChild(task);
+                                break;
+                            default:
+                                alert("Lista inválida. A tarefa não foi adicionada a nenhuma lista.");
+                        }
+                    }
+                });
             }
         });
     });
